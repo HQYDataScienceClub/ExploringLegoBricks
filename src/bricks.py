f5c31f6552e9ac600feb8caa48002417c542c824
Oli Olafsson <oolafsson@healthequity.com>
2018-04-16 12:57:19 -0600

    Add hints to assignment file

--- src/bricks.py
+++ src/bricks.py
@@ -5,13 +5,13 @@ to get a sense of the diversity of lego sets!
 into the ExploringLegoBricks/data directory.
 """
 
-
+import pandas as pd
 
 # Read colors.csv data set into variable `colors`
-
+colors =
 
 # Print the first few rows
-
+print(colors.head())
 
 
 """ 2. Exploring Colors:
@@ -20,8 +20,8 @@ Let us start by understanding the number of colors available.
 """
 
 # How many distinct colors are available?
-
-
+num_colors =
+print(num_colors)
 
 
 """ 3. Transparent Colors:
@@ -32,7 +32,8 @@ of transparent vs. non-transparent colors.
 """
 
 # What is the distribution of colors based on transparency?
-
+colors_summary =
+print(colors_summary)
 
 """ 4. Explore Lego Sets:
 Another interesting dataset available in this database is the sets data.
@@ -42,12 +43,14 @@ Let us use this data to explore how the average number of
 parts in Lego sets has varied over the years.
 """
 
+import matplotlib.pyplot as plt
 
 # Read sets data as `sets`
-
+sets =
 
 # What is the average number of parts by year?
-
+parts_by_year =
+print(parts_by_year)
 
 # Plot trends in average number of parts by year
 
@@ -60,3 +63,5 @@ shipped has varied over the years.
 """
 
 # What is the number of themes shipped by year?
+themes_by_year =
+print(themes_by_year.head())
